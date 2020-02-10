8abb4057ff5e205be927677ebdf44ae291d5a951
Antony Sanders <antony.devon92@gmail.com>
2020-02-10 10:03:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School 
+  attr_accessor :student, :grade
+  attr_reader :s_name, :roster
+ 
+ 
+ 
+ def initialize(name, roster = 0)
+   @name = name
+   @roster = Hash.new
+ end
+ 
+ def school_name=(_s_name)
+   @s_name = s_name
+ end
+ 
+ 
+ 
+def add_student
+  
+end
+ 
+ 
+  
+  
+  
+end
+
